ac844f83ea9f37a04d7b0971b17284826c1c5d39
Gorbunov-Danila <144339126+Gorbunov-Danila@users.noreply.github.com>
2024-02-09 23:32:17 +0300

    Delete individ 1.py

--- individ 1.py
+++
@@ -1,8 +0,0 @@
-import closure_module
-from closure_module import f
-from closure_module import outer_function
-
-if __name__ == "__main__":
-    closure = outer_function(f)
-
-    print(closure(3, 4))
