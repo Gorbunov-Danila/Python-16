b571c3960dd52923ed7c2b75aab6f311f5854791
Gorbunov-Danila <gorbunovdanil345@gmail.com>
2024-02-12 21:35:40 +0300

    4

--- progr/closure_module.py
+++ progr/closure_module.py
@@ -1,20 +1,24 @@
-def outer_function(f):
-    """
-    Внешняя функция
-    """
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
 
-    def inner_function(a, b):
+if __name__ == "__main__": 
+    def outer_function(f):
         """
-        Возвращает строку с вычисленным значением функции f
+        Внешняя функция
         """
-        result = f(a, b)
-        return f"Для значений {a}, {b} функция f(a, b) = {result}"
 
-    return inner_function
+        def inner_function(a, b):
+            """
+            Возвращает строку с вычисленным значением функции f
+            """
+            result = f(a, b)
+            return f"Для значений {a}, {b} функция f(a, b) = {result}"
+
+        return inner_function
 
 
-def f(a, b):
-    """
-    Делит параметры друг на друга и умножает на 2
-    """
-    return a / b * 2
+    def f(a, b):
+        """
+        Делит параметры друг на друга и умножает на 2
+        """
+        return a / b * 2
